7717e96751b2c6aa4b3cf1596f0f3a2b5aec5283
Subhash Vishwakarma <140629565+subhgit02@users.noreply.github.com>
2025-06-01 21:33:37 +0530

    Add files via upload

---
+++ fourth.js
@@ -0,0 +1,20 @@
+const profile = {
+  name: "Charlie",
+  age: 29,
+  address: {
+    city: "San Francisco",
+    zipcode: "94101"
+  }
+};
+const updates = {
+  age: 30,
+  address: {
+    zipcode: "94109",
+    country: "USA"
+  }
+};
+const mergedProfile = Object.assign({}, profile, updates, {
+  address: Object.assign({}, profile.address, updates.address)
+});
+
+console.log(mergedProfile);
