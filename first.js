74c6b5fd7d85112cd6cd578bad3f8c4ee806769e
Subhash Vishwakarma <140629565+subhgit02@users.noreply.github.com>
2025-06-01 21:24:45 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,11 @@
+let age = 25;
+function displayAge() {
+  console.log("Current age:", age);
+}
+function changeAge() {
+  age = 30;
+  console.log("Age updated to:", age);
+}
+displayAge();    
+changeAge();    
+displayAge();
